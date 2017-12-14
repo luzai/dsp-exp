58d2367dac4a18c0755795af62d3fdc7a9028e53
Xinglu Wang <907682447@qq.com>
2017-12-14 12:54:34 +0800

    finish:facelocator

--- PlugIns/FaceLocator/FaceLocator.cpp
+++ PlugIns/FaceLocator/FaceLocator.cpp
@@ -26,7 +26,7 @@ static char THIS_FILE[] = __FILE__;
 //
 //		It is very important that this macro appear in each
 //		function, prior to any calls into MFC.  This means that
-//		it must appear as the first statement within the
+//		it must appear as the first statement within the 
 //		function, even before any object variable declarations
 //		as their constructors may generate calls into the MFC
 //		DLL.
@@ -37,10 +37,10 @@ static char THIS_FILE[] = __FILE__;
 /////////////////////////////////////////////////////////////////////////////
 // CFaceLocatorApp
 BEGIN_MESSAGE_MAP(CFaceLocatorApp, CWinApp)
-//{{AFX_MSG_MAP(CFaceLocatorApp)
-// NOTE - the ClassWizard will add and remove mapping macros here.
-//    DO NOT EDIT what you see in these blocks of generated code!
-//}}AFX_MSG_MAP
+	//{{AFX_MSG_MAP(CFaceLocatorApp)
+		// NOTE - the ClassWizard will add and remove mapping macros here.
+		//    DO NOT EDIT what you see in these blocks of generated code!
+	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 /////////////////////////////////////////////////////////////////////////////
 // CFaceLocatorApp construction
@@ -56,38 +56,39 @@ char sInfo[] = "人脸跟踪-基于彩色信息的人脸分割处理插件";
 bool bLastPlugin = false;
 DLL_EXP void ON_PLUGIN_BELAST(bool bLast)
 {
-	AFX_MANAGE_STATE(AfxGetStaticModuleState()); //模块状态切换
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());//模块状态切换
 	bLastPlugin = bLast;
 }
 DLL_EXP LPCTSTR ON_PLUGININFO(void)
 {
-	AFX_MANAGE_STATE(AfxGetStaticModuleState()); //模块状态切换
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());//模块状态切换
 	return sInfo;
 }
 DLL_EXP void ON_INITPLUGIN(LPVOID lpParameter)
 {
-	AFX_MANAGE_STATE(AfxGetStaticModuleState()); //模块状态切换
-												 //theApp.dlg.Create(IDD_PLUGIN_SETUP);
-												 //theApp.dlg.ShowWindow(SW_HIDE);
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());//模块状态切换
+	//theApp.dlg.Create(IDD_PLUGIN_SETUP);
+	//theApp.dlg.ShowWindow(SW_HIDE);
 }
-DLL_EXP int ON_PLUGINCTRL(int nMode, void *pParameter)
+DLL_EXP int ON_PLUGINCTRL(int nMode,void* pParameter)
 {
 	AFX_MANAGE_STATE(AfxGetStaticModuleState());
 	int nRet = 0;
-	switch (nMode)
+	switch(nMode)
 	{
 	case 0:
-	{
-		//theApp.dlg.ShowWindow(SW_SHOWNORMAL);
-		//theApp.dlg.SetWindowPos(NULL,0,0,0,0,SWP_NOMOVE|SWP_NOSIZE|SWP_FRAMECHANGED);
-	}
-	break;
+		{
+			//theApp.dlg.ShowWindow(SW_SHOWNORMAL);
+			//theApp.dlg.SetWindowPos(NULL,0,0,0,0,SWP_NOMOVE|SWP_NOSIZE|SWP_FRAMECHANGED);
+		}
+		break;
 	}
 	return nRet;
 }
 /****************************************************************************************/
 /*                             人脸检测与定位                                           */
 /****************************************************************************************/
+
 
 void Erosion(aBYTE *tempImg, int width, int height, int N)
 {
@@ -252,7 +253,7 @@ aRect get_bbox(aBYTE *src, int w, int h, int value, aBYTE *dst)
 		{
 			if (src[j * w + i] == value)
 			{
-				if (dst != null)
+				if (!dst)
 					dst[j * w + i] = 255;
 				if (i < minx)
 					minx = i;
@@ -263,24 +264,63 @@ aRect get_bbox(aBYTE *src, int w, int h, int value, aBYTE *dst)
 				if (j > maxy)
 					maxy = j;
 			}
-			else if (dst != null)
+			else if (!dst)
 				dst[j * w + i] = 0;
 		}
 	}
 	aRect res = {minx,miny,maxx-minx,maxy-miny}; 
-	return res 
+	return res;
+}
+void CopyToRect(
+	         aBYTE* ThisImage,//区域图片指针
+			 aBYTE* anImage,//目标图片指针
+			 int W,int H,//区域图片大小
+		     int DestW,int DestH,//目标图片大小
+			 int nvLeft,int nvTop,//区域位置：图片左上角在目标图片的坐标
+			 bool bGray//灰度图片标记，true：灰度；false：彩色。
+					  )
+{
+    aBYTE  * lpSrc = ThisImage;
+    aBYTE  * lpDes = anImage;
+    aBYTE  * lps, * lpd;
+    int h;
+	ASSERT(ThisImage && anImage);
+    ASSERT( nvLeft+W<=DestW && nvTop+H<=DestH && nvLeft>=0 && nvTop>=0 );
+	//Y
+    for(h=0;h<H;h++){
+        lpd = lpDes+(nvTop+h)*DestW+nvLeft;
+        lps = lpSrc+(DWORD)h*W;
+		memcpy(lpd,lps,W);
+    }
+	if( bGray )	return;
+	//U
+	lpSrc += W*H;
+	lpDes += DestW*DestH;
+    for(h=0;h<H;h++){
+        lpd = lpDes+(nvTop+h)*(DestW/2)+nvLeft/2;
+        lps = lpSrc+(DWORD)h*W/2;
+		memcpy(lpd,lps,W/2);
+    }
+	//V
+	lpSrc += W*H/2;
+	lpDes += DestW*DestH/2;
+    for(h=0;h<H;h++){
+        lpd = lpDes+(nvTop+h)*(DestW/2)+nvLeft/2;
+        lps = lpSrc+(DWORD)h*W/2;
+		memcpy(lpd,lps,W/2);
+    }
 }
 
-DLL_EXP void ON_PLUGINRUN(int w, int h, BYTE *pYBits, BYTE *pUBits, BYTE *pVBits, BYTE *pBuffer)
+DLL_EXP void ON_PLUGINRUN(int w,int h,BYTE* pYBits,BYTE* pUBits,BYTE* pVBits,BYTE* pBuffer)
 {
-	//pYBits 大小为w*h
-	//pUBits 和 pVBits 的大小为 w*h/2
-	//pBuffer 的大小为 w*h*4
-	//下面算法都基于一个假设，即w是16的倍数
+//pYBits 大小为w*h
+//pUBits 和 pVBits 的大小为 w*h/2
+//pBuffer 的大小为 w*h*4
+//下面算法都基于一个假设，即w是16的倍数
 
-	AFX_MANAGE_STATE(AfxGetStaticModuleState()); //模块状态切换
-
-	//请编写相应处理程序
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());//模块状态切换
+	
+      //请编写相应处理程序
 	int i, j, max;
 	int pixelnum[256] = {0};
 	int minx, miny, maxx, maxy;
@@ -293,20 +333,22 @@ DLL_EXP void ON_PLUGINRUN(int w, int h, BYTE *pYBits, BYTE *pUBits, BYTE *pVBits
 
 	for (i = 0; i < w * h / 16; i++)
 		tempImg[i] = 255 * BUF->pOtherVars->byHistMap_U[pU[i]] * BUF->pOtherVars->byHistMap_V[pV[i]];
-
-	CopyToRect(tempImg,
-			   pYBits,
-			   160, 120,
-			   640, 480,
-			   480, 1,
-			   true);
-
+	
+	
+	
 	Erosion(tempImg, w / 4, h / 4, 3);
 	Dilation(tempImg, w / 4, h / 4, 3);
 
 	Dilation(tempImg, w / 4, h / 4, 7);
 	Erosion(tempImg, w / 4, h / 4, 7);
-
+	if (bLastPlugin){
+		CopyToRect(tempImg,
+			   pYBits,
+			   160, 120,
+			   640, 480,
+			   480, 0,
+			   true);
+	}
 	RegionLabel(tempImg, w / 4, h / 4);
 	//5人脸区域定位及显示
 	//统计各连通域像素总数
@@ -328,28 +370,37 @@ DLL_EXP void ON_PLUGINRUN(int w, int h, BYTE *pYBits, BYTE *pUBits, BYTE *pVBits
 
 	for (i = 0; i < w * h / 8; i++)
 		BUF->clrBmp_1d8[i] = tempImg[i / 2];
-	aRect rcn = {BUF->rcnFace.left * 2,
-				 BUF->rcnFace.top * 4,
-				 BUF->rcnFace.width * 2,
-				 BUF->rcnFace.height * 4};
 
-	DrawRectangle(
-		pYBits, w, h,
-		rcn,
-		TYUV(59, 15, 10), // todo 0x591510
-		false);
-
-	CopyToRect(BUF->clrBmp_1d8, //源图片指针
-			   pYBits,			//目标图片指针,用displayimage会报错
-			   320, 120,		//区域图片大小
-			   640, 480,		//目标图片大小
-			   1, 360,			//区域位置:图片左上角在目标图片的坐标
-			   false);
+	if (bLastPlugin){
+		
+		aRect rcn = {BUF->rcnFace.left * 2,
+					 BUF->rcnFace.top * 4,
+					 BUF->rcnFace.width * 2,
+					 BUF->rcnFace.height * 4};
+		for (i=0;i<3;i++){
+			DrawRectangle(
+				pYBits, w, h,
+				rcn,
+				TYUV(59, 15, 10),  
+				false);
+			rcn.left+=1;
+			rcn.top+=1;
+			rcn.width-=2;
+			rcn.height-=2;
+		}
+		CopyToRect(BUF->clrBmp_1d8, //源图片指针
+				   pYBits,			//目标图片指针,用displayimage会报错
+				   320, 120,		//区域图片大小
+				   640, 480,		//目标图片大小
+				   0, 0,			//区域位置:图片左上角在目标图片的坐标
+				   false);
+	}
 	myHeapFree(tempImg);
 }
 
 DLL_EXP void ON_PLUGINEXIT()
 {
-	AFX_MANAGE_STATE(AfxGetStaticModuleState()); //模块状态切换
-												 //theApp.dlg.DestroyWindow();
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());//模块状态切换
+	//theApp.dlg.DestroyWindow();
 }
+
--- PlugIns/ImagePrepare/ImagePrepare.cpp
+++ PlugIns/ImagePrepare/ImagePrepare.cpp
@@ -3,11 +3,11 @@
 #include "stdafx.h"
 #include "ImagePrepare.h"
 //
+#include "assert.h"
 #include "BufStruct.h"
 #include "ImageProc.h"
 #include "math.h"
-#include "VFWCAPTURE.h"
-#include "VFWCAPTUREDlg.h"
+
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #undef THIS_FILE
@@ -31,7 +31,7 @@ static char THIS_FILE[] = __FILE__;
 //
 //		It is very important that this macro appear in each
 //		function, prior to any calls into MFC.  This means that
-//		it must appear as the first statement within the
+//		it must appear as the first statement within the 
 //		function, even before any object variable declarations
 //		as their constructors may generate calls into the MFC
 //		DLL.
@@ -42,10 +42,10 @@ static char THIS_FILE[] = __FILE__;
 /////////////////////////////////////////////////////////////////////////////
 // CImagePrepareApp
 BEGIN_MESSAGE_MAP(CImagePrepareApp, CWinApp)
-//{{AFX_MSG_MAP(CImagePrepareApp)
-// NOTE - the ClassWizard will add and remove mapping macros here.
-//    DO NOT EDIT what you see in these blocks of generated code!
-//}}AFX_MSG_MAP
+	//{{AFX_MSG_MAP(CImagePrepareApp)
+		// NOTE - the ClassWizard will add and remove mapping macros here.
+		//    DO NOT EDIT what you see in these blocks of generated code!
+	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 //
 /////////////////////////////////////////////////////////////////////////////
@@ -64,25 +64,25 @@ bool bLastPlugin = false;
 
 DLL_EXP void ON_PLUGIN_BELAST(bool bLast)
 {
-	AFX_MANAGE_STATE(AfxGetStaticModuleState()); //模块状态切换
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());//模块状态切换
 	bLastPlugin = bLast;
 }
 //插件名称
 DLL_EXP LPCTSTR ON_PLUGININFO(void)
 {
-	AFX_MANAGE_STATE(AfxGetStaticModuleState()); //模块状态切换
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());//模块状态切换
 	return sInfo;
 }
 //
 DLL_EXP void ON_INITPLUGIN(LPVOID lpParameter)
-{
-	AFX_MANAGE_STATE(AfxGetStaticModuleState()); //模块状态切换
-												 //theApp.dlg.Create(IDD_PLUGIN_SETUP);
-												 //theApp.dlg.ShowWindow(SW_HIDE);
+{   
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());//模块状态切换
+	//theApp.dlg.Create(IDD_PLUGIN_SETUP);
+	//theApp.dlg.ShowWindow(SW_HIDE);
 }
-DLL_EXP int ON_PLUGINCTRL(int nMode, void *pParameter)
+DLL_EXP int ON_PLUGINCTRL(int nMode,void* pParameter)
 {
-	//模块状态切换
+//模块状态切换
 	AFX_MANAGE_STATE(AfxGetStaticModuleState());
 	int nRet = 0;
 	return nRet;
@@ -91,53 +91,126 @@ DLL_EXP int ON_PLUGINCTRL(int nMode, void *pParameter)
 /****************************************************************************************/
 /*                             摄像头视频流图片截取、重采样等处理                       */
 /****************************************************************************************/
+//DLL_INP void ShowDebugMessage(char* format,...);
 
-// extern "C" _declspec(ddlimport) void myHeapAllocInit(BUF_STRUCT *pBufStruct);
-DLL_INP void myHeapAllocInit(BUF_STRUCT *pBufStruct);
-DLL_INP bool ReSample(aBYTE *ThisImage, int Width, int Height, //源图片指针及大小
-							  int newWidth, int newHeight,			   //目标图片大小
-							  bool InsMode,							   //true：双线性内插法，false：邻近点采样法
-							  bool bGray,							   //true：灰度图片采样，false：彩色图片采样
-							  aBYTE *result							   //目标图片指针
-							  );
+void (*AddMessageToList)(char * message)=NULL;
+
+DLL_EXP void InitMessageOpFunction(void (*AMTL)(char *))
+{
+	AddMessageToList = AMTL;
+}
+
+DLL_EXP void ShowDebugMessage(char* format,...)
+{
+	va_list ap;
+	char sBuffer[1024];
+	va_start(ap, format);
+	vsprintf(sBuffer,format,ap);
+	va_end(ap);
+	if( AddMessageToList )
+		(*AddMessageToList)(sBuffer);
+}
+
+DLL_INP aBYTE *myHeapAlloc(int size);
+
+DLL_INP bool ReSample(aBYTE *ThisImage, int Width, int Height, int newWidth, int newHeight, bool InsMode, bool bGray, aBYTE *result);
+//DLL_INP void CopyToRect(aBYTE *ThisImage,aBYTE *anImage, int W, int H, int DestW, int DestH, int nvLeft, int nvTop,bool bGray);
 
 void copy_img(aBYTE *src_img, aBYTE *dst_img, int w, int h)
 {
 	int sum = w * h;
-	for (int i = 0; i < sum; i++)
-		dst_img[i] = src_img[i];
+	//for (int i = 0; i < sum; i++)
+	//	dst_img[i] = src_img[i];
+	memcpy(dst_img,src_img,sum);
+}
+DLL_EXP void InitFeatureVector(FeatureVector *pThis)
+{
+	int i;
+	ASSERT(pThis);
+	FeatureVector4P *pVector = (FeatureVector4P*)pThis->Vector;
+	pVector->pNL_LeftTop = (FeatureVector4P*)((aBYTE*)pVector+sizeof(FeatureVector4P));
+	pVector->pNL_RightTop = (FeatureVector4P*)((aBYTE*)pVector+2*sizeof(FeatureVector4P));
+	pVector->pNL_LeftBottom = (FeatureVector4P*)((aBYTE*)pVector+3*sizeof(FeatureVector4P));
+	pVector->pNL_RightBottom = (FeatureVector4P*)((aBYTE*)pVector+4*sizeof(FeatureVector4P));
+	pThis->size = sizeof(FeatureVector4P)*5;
+	pVector->nLevels = 2;
+	pVector->pNL_LeftTop->nLevels = 1;
+	pVector->pNL_RightTop->nLevels = 1;
+	pVector->pNL_LeftBottom->nLevels = 1;
+	pVector->pNL_RightBottom->nLevels = 1;
+	for(i=0; i<4; i++)
+	{
+		pVector->pNL_LeftTop->Vector[i].x = pVector->pNL_LeftTop->Vector[i].y = 0;
+		pVector->pNL_RightTop->Vector[i].x = pVector->pNL_RightTop->Vector[i].y = 0;
+		pVector->pNL_LeftBottom->Vector[i].x = pVector->pNL_LeftBottom->Vector[i].y = 0;
+		pVector->pNL_RightBottom->Vector[i].x = pVector->pNL_RightBottom->Vector[i].y = 0;
+	}
 }
 
-void InitTraceObject(TRACE_OBJECT *obj, char *mode)
+void InitTraceObject(TRACE_OBJECT *pThis, char name[8])
 {
-	obj->rcObject.height = obj->rcObject.width=obj->rcObject.top=obj->rcObject.left=0;
-	obj->fvObject->size = sizeof(FeatureVector4P) * 5;
-	// todo
-	int i; 
-	for ( i = 0; i < 4; i++)
-		obj->fvObject.Vector[i] = (0, 0);
-	obj->fvObject_org.size = sizeof(FeatureVector4P) * 5;
-	for ( i = 0; i < 4; i++)
-		obj->fvObject_org.Vector[i] = (0, 0);
-	obj->spdxObj = obj->spdyObj = 0;
-	obj->nMinDist = 0x7fffffff;
-	obj->bBrokenTrace = false;
-	obj->bSaveit = false;
-	obj->nBrokenTimes = 0;
-	//obj->sName = "lz-tr12";
+	memset(&pThis->rcObject, 0, sizeof(aRect));
+	pThis->spdxObj = pThis->spdyObj = 0;
+	pThis->nMinDist = 0x7fffffff;
+	pThis->bBrokenTrace = false;
+	pThis->nBrokenTimes = 0;
+	pThis->bSaveit = false;
+	InitFeatureVector(&pThis->fvObject_org);
+	strncpy(pThis->sName, name, 8);
+	pThis->sName[7] = 0;
 }
 
-DLL_EXP void ON_PLUGINRUN(int w, int h, BYTE *pYBits, BYTE *pUBits, BYTE *pVBits, BYTE *pBuffer)
+DLL_EXP void CopyToRect(
+	         aBYTE* ThisImage,//区域图片指针
+			 aBYTE* anImage,//目标图片指针
+			 int W,int H,//区域图片大小
+		     int DestW,int DestH,//目标图片大小
+			 int nvLeft,int nvTop,//区域位置：图片左上角在目标图片的坐标
+			 bool bGray//灰度图片标记，true：灰度；false：彩色。
+					  )
 {
-	//pYBits 大小为w*h
-	//pUBits 和 pVBits 的大小为 w*h/2
-	//pBuffer 的大小为 w*h*6
-	//下面算法都基于一个假设，即w是16的倍数
-	AFX_MANAGE_STATE(AfxGetStaticModuleState()); //模块状态切换
+    aBYTE  * lpSrc = ThisImage;
+    aBYTE  * lpDes = anImage;
+    aBYTE  * lps, * lpd;
+    int h;
+	ASSERT(ThisImage && anImage);
+    ASSERT( nvLeft+W<=DestW && nvTop+H<=DestH && nvLeft>=0 && nvTop>=0 );
+	//Y
+    for(h=0;h<H;h++){
+        lpd = lpDes+(nvTop+h)*DestW+nvLeft;
+        lps = lpSrc+(DWORD)h*W;
+		memcpy(lpd,lps,W);
+    }
+	if( bGray )	return;
+	//U
+	lpSrc += W*H;
+	lpDes += DestW*DestH;
+    for(h=0;h<H;h++){
+        lpd = lpDes+(nvTop+h)*(DestW/2)+nvLeft/2;
+        lps = lpSrc+(DWORD)h*W/2;
+		memcpy(lpd,lps,W/2);
+    }
+	//V
+	lpSrc += W*H/2;
+	lpDes += DestW*DestH/2;
+    for(h=0;h<H;h++){
+        lpd = lpDes+(nvTop+h)*(DestW/2)+nvLeft/2;
+        lps = lpSrc+(DWORD)h*W/2;
+		memcpy(lpd,lps,W/2);
+    }
+}
 
+
+DLL_EXP void ON_PLUGINRUN(int w,int h,BYTE* pYBits,BYTE* pUBits,BYTE* pVBits,BYTE* pBuffer)
+{
+//pYBits 大小为w*h
+//pUBits 和 pVBits 的大小为 w*h/2
+//pBuffer 的大小为 w*h*6
+//下面算法都基于一个假设，即w是16的倍数
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());//模块状态切换
 	int i, j;
 	BUF_STRUCT *BUF = (BUF_STRUCT *)pBuffer;
-
+    
 	if (BUF->bNotInited)
 	{
 		BUF->colorBmp = pBuffer + sizeof(BUF_STRUCT);
@@ -146,7 +219,7 @@ DLL_EXP void ON_PLUGINRUN(int w, int h, BYTE *pYBits, BYTE *pUBits, BYTE *pVBits
 		BUF->grayBmp_1d16 = BUF->clrBmp_1d8 + w * h / 4;
 		BUF->TempImage1d8 = BUF->grayBmp_1d16 + w * h / 16;
 		BUF->lastImageQueue1d16m8 = BUF->TempImage1d8 + w * h / 8;
-		BUF->pOtherVars = (OTHER_VARS *)(BUF->lastImageQueue1d16m8 + w * h * 2);
+		BUF->pOtherVars = (OTHER_VARS *)(BUF->lastImageQueue1d16m8 + w * h / 2);
 		BUF->pOtherData = (aBYTE *)BUF->pOtherVars + sizeof(OTHER_VARS);
 		for (i = 0; i <= 7; i++)
 			BUF->pImageQueue[i] = BUF->lastImageQueue1d16m8 + i * (w * h / 16);
@@ -176,6 +249,7 @@ DLL_EXP void ON_PLUGINRUN(int w, int h, BYTE *pYBits, BYTE *pUBits, BYTE *pVBits
 				BUF->pOtherVars->byHistMap_V[i] = 0;
 		}
 
+		ShowDebugMessage("hi\n");
 		InitTraceObject(&BUF->pOtherVars->objNose, "nosetrac");
 		InitTraceObject(&BUF->pOtherVars->objLefteye, "Leyetrac");
 		InitTraceObject(&BUF->pOtherVars->objRighteye, "Reyetrac");
@@ -186,8 +260,8 @@ DLL_EXP void ON_PLUGINRUN(int w, int h, BYTE *pYBits, BYTE *pUBits, BYTE *pVBits
 		myHeapAllocInit(BUF);
 		BUF->bNotInited = false;
 	}
-
-	BUF->displayImage = pYBits;
+   
+    BUF->displayImage = pYBits;
 
 	BYTE *p = BUF->colorBmp;
 	copy_img(pYBits, p, w, h);
@@ -195,32 +269,35 @@ DLL_EXP void ON_PLUGINRUN(int w, int h, BYTE *pYBits, BYTE *pUBits, BYTE *pVBits
 	copy_img(pUBits, p, w, h / 2);
 	p += w * h / 2;
 	copy_img(pVBits, p, w, h / 2);
-
+	
 	ReSample(BUF->colorBmp, w, h, w / 2, h / 4, true, false, BUF->clrBmp_1d8);
-
 	ReSample(BUF->grayBmp, w, h, w / 4, h / 4, true, true, BUF->grayBmp_1d16);
-
+	
+	
 	if (bLastPlugin)
 	{
-		// todo blastplugin? copytorect stdafx
-		CopyToRect(BUF->clrBmp_1d8, //源图片指针
-				   pYBits,			//目标图片指针,用displayimage会报错
-				   320, 120,		//区域图片大小
-				   640, 480,		//目标图片大小
-				   1, 1,			//区域位置:图片左上角在目标图片的坐标
-				   false);
+		ShowDebugMessage("hi\n");
 		CopyToRect(BUF->grayBmp_1d16, //源图片指针
 				   pYBits,			  //目标图片指针
 				   160, 120,		  //区域图片大小
 				   640, 480,		  //目标图片大小
-				   480, 1,			  //区域位置:图片左上角在目标图片的坐标
+				   0, 0,			  //区域位置:图片左上角在目标图片的坐标
 				   true);
+		CopyToRect(BUF->clrBmp_1d8, //源图片指针
+				   pYBits,			//目标图片指针,用displayimage会报错
+				   320, 120,		//区域图片大小
+				   640, 480,		//目标图片大小
+				   w/2, 0,			//区域位置:图片左上角在目标图片的坐标
+				   false);
+		
 	}
+    
 }
+
 
 
 DLL_EXP void ON_PLUGINEXIT()
 {
-	AFX_MANAGE_STATE(AfxGetStaticModuleState()); //模块状态切换
-												 //theApp.dlg.DestroyWindow();
+	AFX_MANAGE_STATE(AfxGetStaticModuleState());//模块状态切换
+	//theApp.dlg.DestroyWindow();
 }
